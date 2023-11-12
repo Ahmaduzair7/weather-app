f40f063959f7b41dbeb846eeb92e23de057b54f9
Ahmaduzair7 <crcformula@gmail.com>
2023-11-12 23:50:43 +0500

    Finish

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const apiKey = "472c5354d922ace440baa38a72547fde";
+ const apiKey = "472c5354d922ace440baa38a72547fde";
 const btn = document.getElementById('btn');
 
 
@@ -10,32 +10,31 @@ const callLocationAPI = function(city) {
     .then(json => {
       var lat = json[0].lat; 
       var lon = json[0].lon; 
-      console.log(lat);
-      console.log(lon);
-       weather(lat,lon);
+     
+       weather(lat,lon,city);
      
     })
     
 };
 
-const weather = function(lat,lon){
+const weather = function(lat,lon,city){
   fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=472c5354d922ace440baa38a72547fde`)
   .then(res => res.json())
   .then(json=> {
-    console.log(json.main.temp)
     
-    const name  = document.querySelector('input').value;
-    const { icon, description } = json.weather[0]
-    const { temp, humidity } = json.main
-    const { speed } = json.wind
-    document.querySelector('.city').innerText = 'Weather in ' + name
-    document.querySelector('.icon').src =
-    'https://openweathermap.org/img/wn/' + icon + '.png'
-    document.querySelector('.description').innerText = description
-    document.querySelector('.temp').innerText = temp + '°C'
-    document.querySelector('.humidity').innerText =
-      'Humidity: ' + humidity + '%'
-    document.querySelector('.wind').innerText = 'Wind speed: ' + speed + ' km/h'
+    const name  = city;
+    const  icon = json.weather[0].icon
+    const  description = json.weather[0].description
+    const  temp = json.main.temp
+    const  humidity = json.main.humidity
+    const  speed  = json.wind
+
+    document.querySelector('.city').innerText = 'Weather in ' + name;
+    document.querySelector('.icon').src = 'https://openweathermap.org/img/wn/' + icon + '.png';
+    document.querySelector('.description').innerText = description;
+    document.querySelector('.temp').innerText = temp + '°C';
+    document.querySelector('.humidity').innerText = 'Humidity: ' + humidity + '%';
+    document.querySelector('.wind').innerText = 'Wind speed: ' + speed + ' km/h';
     
   })
 }
@@ -44,7 +43,9 @@ btn.addEventListener('click', function() {
   const cityInput = document.querySelector('input').value;
   callLocationAPI(cityInput);
 });
+
 const input  = document.querySelector('input');
+
 input.addEventListener('keyup', function(event) {
   if(event.key == 'Enter'){
     const cityInput = document.querySelector('input').value;
@@ -57,7 +58,18 @@ navigator.geolocation.getCurrentPosition((position) => {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
 
-  weather(lat,lon);
+  fetch(`http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=5&appid=${apiKey}`)
+    .then(res => res.json())
+    .then(json => {
+      
+      const city = json[0].name;
+      
+      
+       weather(lat,lon,city);
+     
+    })
 
-})
-/* https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&units=metric&appid=472c5354d922ace440baa38a72547fde */
+ 
+
+});
+
